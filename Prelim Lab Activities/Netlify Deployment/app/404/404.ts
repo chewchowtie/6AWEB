1f2cb98c64eacb2fd76890683e6c60203825d542
Meggy <146339540+chewchowtie@users.noreply.github.com>
2025-12-03 15:42:13 +0800

    Add files via upload

---
+++ Prelim Lab Activities/Netlify Deployment/app/404/404.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-404',
+  imports: [],
+  templateUrl: './404.html',
+  styleUrl: './404.css',
+})
+export class Pagenotfound {
+
+}
