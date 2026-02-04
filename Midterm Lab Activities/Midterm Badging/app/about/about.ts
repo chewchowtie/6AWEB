8548782b0b6d5809dff6e81f8d1614f81dad2404
Meggy <146339540+chewchowtie@users.noreply.github.com>
2026-02-04 15:44:53 +0800

    Add files via upload

---
+++ Midterm Lab Activities/Midterm Badging/app/about/about.ts
@@ -0,0 +1,12 @@
+import { DatePipe } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  imports: [DatePipe],
+  templateUrl: './about.html',
+  styleUrl: './about.css',
+})
+export class About {
+  currentDate: Date = new Date();
+}
